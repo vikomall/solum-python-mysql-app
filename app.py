1bd5478b32f5551fe89d3c2b64e759593906fad6
Murali Allada <murali.allada@rackspace.com>
2014-12-10 17:39:32 -0600

    read from db

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 import os
+import random
 
 import cherrypy
 from jinja2 import Environment, FileSystemLoader
-
+import MySQLdb
 
 ENV = Environment(loader=FileSystemLoader('templates'))
 
 
 def get_params():
-    params = {'key': os.environ.get('key', ''),
+    params = {'dbname': os.environ.get('dbname', ''),
               'user': os.environ.get('user', ''),
-              'password': os.environ.get('password', '')}
+              'password': os.environ.get('password', ''),
+              'dbhost': os.environ.get('dbhost', ''}
     return params
 
 
@@ -19,7 +21,21 @@ class Root(object):
     def index(self):
         tmpl = ENV.get_template('index.html')
         params = get_params()
-        return tmpl.render(params)
+        db = MySQLdb.connect(params['dbhost'],
+                             params['user'],
+                             params['password'],
+                             params['dbname'])
+        cursor = db.cursor()
+        sql = "SELECT * FROM people"
+
+        try:
+          cursor.execute(sql)
+          results = cursor.fetchall()
+          name = random.choice(results)
+        except:
+          print "Error: unable to fecth data"
+
+        return tmpl.render(name)
 
 cherrypy.config.update({'server.socket_host': '0.0.0.0',
                         'server.socket_port': int(os.environ.get('PORT', '5000'))})
